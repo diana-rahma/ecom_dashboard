a303d6d55d3bc2da8c06cddf15124965c2993690
diana-rahma <dianarhm12@gmail.com>
2022-08-10 11:24:25 -0700

    up

--- src/App.js
+++ src/App.js
@@ -2,14 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 import {Button} from 'react-bootstrap';
 import Header from './Header';
+import {BrowserRouter, Route} from 'react-router-dom';
+import login from './login';
+import register from './register';
+import AddProducts from './AddProducts';
+import UpdateProducts from './UpdateProducts';
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <Header/>
       <h1>E-Commerce Dashboard</h1>
-      <button>Simple HTML Button</button>
-      <Button>Bootstrap button</Button>
+      <Route path="/login">
+        <login/>
+      </Route>
+      </BrowserRouter>
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,17 +1,19 @@
 import {Navbar} from 'react-bootstrap';
 import {Container} from 'react-bootstrap';
 import {Nav} from 'react-bootstrap';
+import {Link} from 'react-router-dom';
 
 function Header() {
     return (
         <div>
             <Navbar bg="dark" variant="dark">
                 <Container>
-                    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                    <Nav className="me-auto">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#features">Features</Nav.Link>
-                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    <Navbar.Brand href="#home">e-Commerce</Navbar.Brand>
+                    <Nav className="me-auto navbar-wrapper">
+                        <Link to="/login">Login</Link>
+                        <Link to="/register">Register</Link>
+                        <Link to="/add">Add Products</Link>
+                        <Link to="/update">Update Products</Link>
                     </Nav>
                 </Container>
             </Navbar>
