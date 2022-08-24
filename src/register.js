568e0d3761cd54f0bf57306b1979b7b1235d1503
diana-rahma <dianarhm12@gmail.com>
2022-08-24 11:39:14 -0700

    up

--- src/register.js
+++ src/register.js
@@ -1,8 +1,26 @@
+import React, { useState } from "react";
+
 function Register() {
-    return(
-        <div>
+    const [name, setName] = useState("");
+    const [password, setPassword] = useState("");
+    const [email, setEmail] = useState("");
+
+    function signUp(){
+        let item={name,password,email};
+        console.warn(name,password,email);
+        fetch("localhost:8000/api/register") 
+    }
+    return (
+        <div className="col-sm-6 offset-sm-3">
             <h1>Halaman Register</h1>
-        </div> 
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)} className="form-control" placeholder="Name" />
+            <br />
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="form-control" placeholder="Password" />
+            <br />
+            <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} className="form-control" placeholder="Email" />
+            <br />
+            <button onClick={signUp()} className="btn-primary">Sign Up</button>
+        </div>
     )
 }
 
