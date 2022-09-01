ca290ff6587a42e1299ca33859538bd86245f912
diana-rahma <dianarhm12@gmail.com>
2022-09-01 12:20:32 -0700

    add

--- src/AddProducts.js
+++ src/AddProducts.js
@@ -1,9 +1,27 @@
 import Header from "./Header";
+import { useState } from 'react';
+
 function AddProducts() {
+    const [name, setName]=useState("");
+    const [price, setPrice]=useState("");
+    const [description, setDescription]=useState("");
+    const [file, setFile]=useState("");
     return(
         <div>
             <Header/>
-            <h1>Halaman Add Products</h1>
+            <h1>Add Products</h1>
+            <div className="col-sm-6 offset-sm-3">
+                <br/>
+                <input type="text" className="form-control" placeholder="Name"></input>
+                <br/>
+                <input type="text" className="form-control" placeholder="Price"></input>
+                <br/>
+                <input type="text" className="form-control" placeholder="Description"></input>
+                <br/>
+                <input type="file" className="form-control" placeholder="File"></input>
+                <br/>
+                <button className="btn btn-primary">Add Product</button>
+            </div>
         </div> 
     );
 }
